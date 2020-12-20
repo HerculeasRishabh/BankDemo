bd2311877cf993ce716c9df93ac1421d73e365a4
rishabhojha <rishabh.education.123@gmail.com>
2020-12-20 13:17:59 +0530

    Committing loan related enhancements

--- Bank/loan/models.py
+++ Bank/loan/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from customUser.models import MyUser
+from django.utils import timezone
 
 class UserLoan( models.Model ):
 
@@ -19,6 +20,20 @@ class UserLoan( models.Model ):
     amount = models.FloatField()
     duration =  models.IntegerField(choices=DURATION_CHOICES, default=3)
     rate_of_interest = models.IntegerField(choices=INTEREST_RATE_CHOICES, default=5)
+    date_applied = models.DateField(default=timezone.now)
 
     def __str__( self ):
         return f'{self.user.username} - {self.amount}'
+
+    @property
+    def compound_interest( self ):
+        ''' By defauly we conpound every quater '''
+        compounding_period = self.duration/3
+        ci_amount = self.amount*( 1 + self.rate_of_interest/(compounding_period*100) )**self.duration
+        return ci_amount
+
+    @property
+    def due_date( self ):
+        ''' End date of loan '''
+        from dateutil.relativedelta import relativedelta
+        return self.date_applied + relativedelta(months=self.duration)
--- Bank/loan/views.py
+++ Bank/loan/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import LoanForm
 from django.contrib import messages
+from .models import UserLoan
 
 @login_required
 def loan( request ):
+    loans = request.user.userloan_set.all()
     if request.method == "POST":
         loan_form = LoanForm(request.POST)
         if loan_form.is_valid():
@@ -15,11 +17,12 @@ def loan( request ):
                                                     )
             new_loan.save()
             messages.success(request, f'Loan request successful!')
-            return redirect('home')
+            return redirect('loan')
     else:
         loan_form = LoanForm()
     
     context = {
         'form': loan_form,
+        'loans' : loans,
     }
     return render(request, "loan/loan_application.html", context)
